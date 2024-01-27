0949513265e5543d55850a3376c74887bed2a4ce
giorgio <giorgio.programmer@gmail.com>
2024-01-27 17:27:12 +0100

    first page done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,7 +11,15 @@ const navigateToService = (path) => {
 
     return (
         <div className="home-page">
-        <JumbotronComponent title="button counter" description="button counter" imageCard="luna.png" onClick={() => navigateToService('/button-counter')} />
+            <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="min-w-0 flex-1">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            Book management
+        </h2>
+        </div>
+        </div>
+
+        <JumbotronComponent title="Contatore" description="conta il numero dei click" imageCard="luna.png" onClick={() => navigateToService('/button-counter')} />
         <JumbotronComponent title="Streamyard" description="" onClick={() => navigateToService('/streamyard')} />
         <JumbotronComponent title="YouTube" description="" onClick={() => navigateToService('/youtube')} />
         <JumbotronComponent title="Dacast" description="" onClick={() => navigateToService('/dacast')} />
