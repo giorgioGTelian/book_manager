c07c2b3b463ca00bf1793d5004d9c672ba840448
giorgioGTelian <giorgio.programmer@gmail.com>
2024-01-29 10:08:58 +0100

    fixing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,12 @@ import { Outlet } from 'react-router-dom';
 // The Home component is a functional component used to structure the main page layout
 const Home = () => {
   return (
+    <>
     <main>
         <Header />
         <Outlet /> {/* Outlet component renders the current route's child routes */}
     </main>
+    </>
   )
 }
 
